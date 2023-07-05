b6d892e1d446c1dfe5365363eb7de2f70e2a2593
Sunil Kumar Korlapu <korlapusunilkumar@gmail.com>
2023-07-05 16:54:12 -0400

    AWS VPC Changes

--- environment/main.tf
+++ environment/main.tf
@@ -7,3 +7,39 @@ terraform {
   }
   required_version = "~> 1.0"
 }
+
+resource "aws_vpc" "vpc_clarks_ice_cream" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Application_name = "Clarks_Ice_Cream"
+      Create_by = "Terraform"
+      resource = "VPC"
+    }
+  
+}
+
+resource "aws_subnet" "Clarks_Ice_Cream_Public_Subnet" {
+    count = length(var.public_subnet_cidr)
+    vpc_id = aws_vpc.vpc_clarks_ice_cream
+    cidr_block = element(var.public_subnet_cidr,count.index)
+    tags = {
+      Application_name = "Clarks_Ice_Cream"
+      Create_by = "Terraform"
+      resource = "VPC.Public.Subnet"
+      Name = "CIC VPC Public Subnet $(count.index +1)" 
+    }
+  
+}
+
+resource "aws_subnet" "Clarks_Ice_Cream_Private_Subnet" {
+    count = length(var.private_subnet_cidr)
+    vpc_id = aws_vpc.vpc_clarks_ice_cream
+    cidr_block = element(var.private_subnet_cidr,count.index)
+    tags = {
+      Application_name = "Clarks_Ice_Cream"
+      Create_by = "Terraform"
+      resource = "VPC.Private.Subnet"
+      Name = "CIC VPC Private Subnet $(count.index +1)" 
+    }
+  
+}
---
+++ environment/variables.tf
@@ -0,0 +1,12 @@
+variable "public_subnet_cidr" {
+    type = list(string)
+    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    description = "Public Subnet CIDR Values"
+}
+
+
+variable "private_subnet_cidr" {
+    type = list(string)
+    default = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+    description = "Private Subnet CIDR Values"
+}
