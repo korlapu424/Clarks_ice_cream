24b966ab46bf1c7d8bd125914cd9529245200779
Sunil Kumar Korlapu <korlapusunilkumar@gmail.com>
2023-07-06 13:53:53 -0400

    Working VPC

--- environment/variables.tf
+++ environment/variables.tf
@@ -8,6 +8,14 @@ variable "public_subnet_cidr" {
 
 variable "private_subnet_cidr" {
     type = list(string)
-    default = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+    default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
     description = "Private Subnet CIDR Values"
 }
+
+variable "azs" {
+    type = list(string)
+    description = "Availability Zones"
+    default = [ "us-east-1a","us-east-1b","us-east-1c" ]
+  
+}
+
