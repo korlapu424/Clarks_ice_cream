b4c92deb0cb30a87774bae52b7cbc4bb5fa3cb1e
Sunil Kumar Korlapu <korlapusunilkumar@gmail.com>
2023-07-07 14:48:10 -0400

    push the chnages

--- environment/main.tf
+++ environment/main.tf
@@ -11,4 +11,8 @@ terraform {
 module "vpc-module-cic" {
     source = "./terraform-modules/vpc-module-cic"   
     cic_vpc_id = "10.0.0.0/16"
+    region = "us-east-1"
+    private_subnet_cidr = ["10.0.1.0/24", "10.0.2.0", "10.0.3.0" ]
+    public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0", "10.0.3.0" ]
+    azs = ["us-east-1a","us-east-1b","us-east-1c"]
 }
