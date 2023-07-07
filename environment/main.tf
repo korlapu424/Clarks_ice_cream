5761b9ed126186fc115f4632258c9cc243bb243f
Sunil Kumar Korlapu <korlapusunilkumar@gmail.com>
2023-07-07 15:03:39 -0400

    new changes for module inputs

--- environment/main.tf
+++ environment/main.tf
@@ -12,7 +12,7 @@ module "vpc-module-cic" {
     source = "./terraform-modules/vpc-module-cic"   
     cic_vpc_id = "10.0.0.0/16"
     region = "us-east-1"
-    private_subnet_cidr = ["10.0.1.0/24", "10.0.2.0", "10.0.3.0" ]
-    public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0", "10.0.3.0" ]
+    private_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+    public_subnet_cidr = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24" ]
     azs = ["us-east-1a","us-east-1b","us-east-1c"]
 }
--- environment/terraform-modules/vpc-module-cic/variables.tf
+++ environment/terraform-modules/vpc-module-cic/variables.tf
@@ -1,25 +1,27 @@
 # https://spacelift.io/blog/terraform-aws-vpc
-variable "public_subnet_cidr" {
-    type = list(string)
-    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    description = "Public Subnet CIDR Values"
+
+
+variable "cic_vpc_id" {
+    type = string
+  
 }
 
 
+variable "region" {
+    type = string
+    default = "us-east-1"
+ }
+
 variable "private_subnet_cidr" {
     type = list(string)
-    default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    description = "Private Subnet CIDR Values"
+  
+}
+
+variable "public_subnet_cidr" {
+    type = list(string)
 }
 
 variable "azs" {
     type = list(string)
-    description = "Availability Zones"
-    default = [ "us-east-1a","us-east-1b","us-east-1c" ]
-  
-}
-
-variable "cic_vpc_id" {
-    type = string
   
 }
